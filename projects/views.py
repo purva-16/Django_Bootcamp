cc45941eaa13c8e51cb58205f4ce0ac91f487fb2
purva-16 <pb2330@srmist.edu.in>
2023-12-14 14:46:17 +0530

    2nd commit - views and URLs

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('this is our projects page')
+
+def project(request,pk):
+    return HttpResponse('projects page:'+ str(pk))
 
 # Create your views here.
