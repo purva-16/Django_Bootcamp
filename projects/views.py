f9dfd1342f914b0dcd81deeefd3a337a8de34825
purva-16 <pb2330@srmist.edu.in>
2023-12-23 18:30:54 +0530

    3rd branch 3rd commit- update project

--- projects/views.py
+++ projects/views.py
@@ -32,3 +32,23 @@ def createproject(request):
     context = {'form': form}
 
     return render (request, 'projects/project-form.html', context)
+
+
+def updateProject(request,pk):
+    context = {}
+    project = Project.objects.get(id=pk)
+    form= ProjectForm(instance=project)
+    template = 'projects/project-form.html'
+
+    if request.method == 'POST':
+      form = ProjectForm(request.POST, instance=project)
+      if form.is_valid():
+          form.save()
+          return redirect('projects')
+
+
+
+    context = {'form': form}
+
+
+    return render(request, template, context)
