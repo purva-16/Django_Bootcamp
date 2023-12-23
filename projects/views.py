8d12ca76fc9b49fee74ddc37d6bd84ae9e719385
purva-16 <pb2330@srmist.edu.in>
2023-12-23 17:10:52 +0530

    3rd branch 2nd commit-Create project

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
@@ -17,6 +17,17 @@ def project(request,pk):
 
 def createproject(request):
     form = ProjectForm()
+    
+    if request.method == "POST":
+       form = ProjectForm(request.POST)
+       if form.is_valid():
+           form.save()
+           return redirect('projects')
+       
+
+
+     
+
 
     context = {'form': form}
 
