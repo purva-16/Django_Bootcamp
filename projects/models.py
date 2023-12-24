be1662fdf146ff7421427a6df5b387aa14126a25
purva-16 <pb2330@srmist.edu.in>
2023-12-24 15:39:07 +0530

    4th branch 1st commit-user uploaded images

--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
    #owner
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    #featured_image=
+    featured_image=models.ImageField(null=True, blank=True)
     demo_link = models.CharField(max_length=1000, null=True, blank=True)
     source_link = models.CharField(max_length=200, null=True, blank=True)
     vote_total = models.IntegerField(default=0)
@@ -20,6 +20,14 @@ class Project(models.Model):
     
     def __str__(self):
         return self.title
+    
+    @property 
+    def imageURL(self):
+        try:
+            img= self.featured_image.url
+        except:
+            img = ''
+        return  img 
     
 class Review(models.Model):
 
@@ -48,6 +56,6 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
-     
+      
     
 
